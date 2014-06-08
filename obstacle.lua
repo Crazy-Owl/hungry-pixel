3f4661fead747a4faf3f64169edc00e4f7238cab
Crazy-Owl <stalker401@gmail.com>
2014-06-08 13:23:23 +0400

    added basic obstacles code

---
+++ obstacle.lua
@@ -0,0 +1,25 @@
+Obstacle = {}
+Obstacle.__index = Obstacle
+setmetatable(Obstacle, {
+    __call = function(cls, x, y, w, h)
+        return cls.new(x, y, w, h)
+    end
+})
+
+function Obstacle.new(x, y, w, h)
+    local self = setmetatable({
+        x = x,
+        y = y,
+        w = w,
+        h = h
+    }, Obstacle)
+end
+
+function Obstacle:update()
+    return
+end
+
+function Obstacle:draw()
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+end
