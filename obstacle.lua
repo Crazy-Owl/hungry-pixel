dd0e88298e334b69a32be903ccf5e8a5d6021a46
Crazy-Owl <stalker401@gmail.com>
2014-06-22 13:36:22 +0400

    added movement to obstacle code

--- obstacle.lua
+++ obstacle.lua
@@ -1,3 +1,5 @@
+require "util"
+
 Obstacle = {}
 Obstacle.__index = Obstacle
 setmetatable(Obstacle, {
@@ -12,7 +14,11 @@ function Obstacle.new(x, y, w, h)
         y = y,
         w = w,
         h = h,
-        type = "obstacle"
+        type = "obstacle",
+        kind = "stationary"
+        speed = {0, 0},
+        destination = nil,
+        dt = 0
     }, Obstacle)
     return self
 end
@@ -23,8 +29,35 @@ function Obstacle.random()
     return Obstacle.new(x, y, currentDifficulty.obstacleWidth, currentDifficulty.obstacleHeight)
 end
 
-function Obstacle:update()
-    return
+function Obstacle:setRandomDestination()
+   local x = math.random(0, windowHeight)
+   local y = math.random(0, windowHeight)
+   self.destination = {x, y}
+end
+
+function Obstacle:update(dt)
+   if self.kind == "stationary" then
+      return
+   end
+   if self.destination ~= nil then
+      local dx = self.destination[1] - self.X
+      local dy = self.destination[2] - self.y
+      if dx >= dy then
+         self.speed = {sign(dx) * 1, sign(dy) * math.abs(dy / dx)}
+      else
+         self.speed = {sign(dy) * math.abs(dx / dy), sign(dy) * 1}
+      end
+      self.x = self.x + self.speed[1] * 100 -- TODO: speed should be set in settings
+      self.y = self.y + self.speed[2] * 100
+   end
+   if self.kind == "moving" then
+      self.dt = self.dt + dt
+      if self.dt > 10 then -- TODO: threshold should be set in settings
+         self.dt = 0
+         self:setRandomDestination()
+      end
+   end
+   return
 end
 
 function Obstacle:draw()
