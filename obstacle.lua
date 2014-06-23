8274ebeb60c3223a8d3a2392323b9f9c2fa10de9
Crazy-Owl <stalker401@gmail.com>
2014-06-23 22:39:55 +0400

    work-in-progress obstacle code commit

--- obstacle.lua
+++ obstacle.lua
@@ -15,7 +15,7 @@ function Obstacle.new(x, y, w, h)
         w = w,
         h = h,
         type = "obstacle",
-        kind = "stationary"
+        kind = "stationary",
         speed = {0, 0},
         destination = nil,
         dt = 0
@@ -26,13 +26,32 @@ end
 function Obstacle.random()
     local x = math.random(0, windowHeight)
     local y = math.random(0, windowHeight)
+    local state = math.random(2)
+    if state > 1 then
+       local obst = Obstacle.new(x, y, currentDifficulty.obstacleWidth, currentDifficulty.obstacleHeight)
+       obst.kind = "moving"
+       obst:setRandomDestination()
+       return obst
+    end
     return Obstacle.new(x, y, currentDifficulty.obstacleWidth, currentDifficulty.obstacleHeight)
 end
 
 function Obstacle:setRandomDestination()
-   local x = math.random(0, windowHeight)
-   local y = math.random(0, windowHeight)
+   local dx = 0
+   local dy = 0
+   local x, y
+   while (dx < 10) or (dy < 10) do
+      x = math.random(0, windowHeight)
+      y = math.random(0, windowHeight)
+      dx = x - self.x
+      dy = y - self.y
+   end
    self.destination = {x, y}
+   if dx >= dy then
+      self.speed = {sign(dx) * 1, sign(dy) * math.abs(dy / dx)}
+   else
+      self.speed = {sign(dy) * math.abs(dx / dy), sign(dy) * 1}
+   end
 end
 
 function Obstacle:update(dt)
@@ -40,15 +59,8 @@ function Obstacle:update(dt)
       return
    end
    if self.destination ~= nil then
-      local dx = self.destination[1] - self.X
-      local dy = self.destination[2] - self.y
-      if dx >= dy then
-         self.speed = {sign(dx) * 1, sign(dy) * math.abs(dy / dx)}
-      else
-         self.speed = {sign(dy) * math.abs(dx / dy), sign(dy) * 1}
-      end
-      self.x = self.x + self.speed[1] * 100 -- TODO: speed should be set in settings
-      self.y = self.y + self.speed[2] * 100
+      self.x = self.x + self.speed[1] * dt * 50 -- TODO: speed should be set in settings
+      self.y = self.y + self.speed[2] * dt * 50
    end
    if self.kind == "moving" then
       self.dt = self.dt + dt
