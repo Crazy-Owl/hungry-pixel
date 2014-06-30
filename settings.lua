8ff1d5d4e37621948bcb2055e66e607088c41de0
Crazy-Owl <stalker401@gmail.com>
2014-06-30 18:58:43 +0400

    added moderate difficulty to game

--- settings.lua
+++ settings.lua
@@ -15,7 +15,7 @@ easyDifficulty = {
     startingHeroWidth = 1,
     startingHeroHeight = 1,
     startingHeroSpeed = {0, 0},
-    startingHeroSize = 4,
+    startingHeroSize = 8,
     heroVelocity = 250,
     heroMaxVelocityChange = 200,
     heroSlowing = 0.4,
@@ -34,5 +34,32 @@ easyDifficulty = {
     objectHeight = 10,
     obstacleMinSize = 20,
     obstacleMaxSize = 35,
+    displayRuler = true
+}
+
+moderateDifficulty = {
+    -- Hero section
+    startingHeroWidth = 1,
+    startingHeroHeight = 1,
+    startingHeroSpeed = {0, 0},
+    startingHeroSize = 4,
+    heroVelocity = 300,
+    heroMaxVelocityChange = 225,
+    heroSlowing = 0.6,
+    heroTimeToShrink = 9,
+    growRate = 2,
+    shrinkRate = 4,
+    minimumWidth = 2,
+    minimumHeight = 2,
+    maxSize = 350,
+    -- gameplay section
+    objectAppearRange = {4, 8},
+    obstacleAppearRange = {12, 26},
+    obstacleBaseTime = 8,
+    objectLifespan = {6, 13},
+    objectWidth = 10,
+    objectHeight = 10,
+    obstacleMinSize = 20,
+    obstacleMaxSize = 45,
     displayRuler = true
 }
